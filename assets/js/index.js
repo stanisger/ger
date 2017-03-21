0e7b9548e6804b64bbd50676cf6dd44112356a89
Gersain <gersigner@hotmail.com>
2017-03-21 16:34:02 -0600

    iniciando comentarios

---
+++ assets/js/index.js
@@ -0,0 +1,82 @@
+
+//∆∆ BTN COLLPASE
+ $(".button-collapse").sideNav();
+
+ //∆∆ MODAL
+ $('.modal').modal();
+
+     $('.modal').modal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .72, // Opacity of modal background
+      inDuration: 550, // Transition in duration
+      outDuration: 550, // Transition out duration
+      ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+    
+         $('.modal').dblclick(function() {
+                  $('.modal').modal('close');
+          }); 
+      }
+     }
+    )
+
+ //∆∆ TABS 
+  var masonryOptions = {
+      itemSelector: '.grid-item',
+    };
+  var $grid = $('.grid').masonry( masonryOptions );
+
+  $("ul.tabs").tabs({
+   onShow: function() {
+          $grid.masonry();
+        },
+
+        transitionDuration: 0
+
+  });
+
+
+
+
+
+// //∆∆ Modal&Objetos
+// var modalSelect = $('.modal');
+// var computer = $('#animate1');
+
+// //∆∆ Altura de Objetos (computer)
+// var objetHeight = function height(objet){  
+//       return  objet.outerHeight();   
+//  }
+
+
+// //∆∆ Scroll (modalSelect)
+// var pixels = function height(obj){
+// 	return obj.scrollTop();
+// }
+
+// $(document).scroll(function(){
+
+
+
+// 	// function ecuation(height){
+// 	//     return (height + pixels(modalSelect)) / height;
+// 	//   }
+    
+
+
+//  //    var animation = function animation(multi){
+//  //       return ecuation(objetHeight(computer)) * multi;
+        
+//  //    }
+
+//  //    if(pixels(modalSelect) >= 1){
+//  //        computer.css({ 
+//  //            "top" : animation(10)  + "px", 
+//  //            "display" : "inherit"
+//  //               }
+            
+//  //         );
+//  //    }
+
+// });
+	
+
